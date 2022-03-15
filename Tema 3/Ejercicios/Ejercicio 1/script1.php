09a97e9270a2ec10e31c75c83133a8342933fe1d
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2022-03-15 13:32:49 +0100

    Delete script1.php

--- Tema 3/Ejercicios/Ejercicio 1/script1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  "<form action='segundoScript.php' method='post'>
-    Name: <input type='text' name='name'><br>
-    E-mail: <input type='text' name='email'><br>
-    <input type='submit'>
-  </form>"
-
-?>
