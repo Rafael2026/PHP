8ce80a2e82cefbb6922039b396ec5f4f2f438f94
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2022-03-15 13:13:14 +0100

    Optimize exercise1 unit2

--- Tema 2/Ejercicios/Ejercicio1.php
+++ Tema 2/Ejercicios/Ejercicio1.php
@@ -1,5 +1,5 @@
 <?php
-  echo "Nombre: " ."Rafael Aguilar Muñoz<br>";
-  echo "Direccion: C/ Ceres, 8<br>";
-  echo "Telefono: 609213851";
+  echo "Name: Rafael Aguilar Muñoz<br>";
+  echo "Address: C/ Ceres, Nº 8<br>";
+  echo "Phone: 609213851";
 ?>
