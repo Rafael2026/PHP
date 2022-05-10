9b944ef056cc15931a2167eb30712f14d97c849a
Rafael Aguilar Muñoz <rafaaguilarmunoz@gmail.com>
2022-05-10 19:25:08 +0200

    Update Ejercicio1.php

--- Tema 4/Ejercicios/Ejercicio1.php
+++ Tema 4/Ejercicios/Ejercicio1.php
@@ -7,7 +7,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Ejercicio 1</title>
-  <link href="https://github.com/Rafael2026/learn_bootstrap/blob/main/bootstrap/css/bootstrap.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
@@ -17,7 +18,6 @@
     require_once("Series.php");
 
     for ($i = 0; $i < count($series); $i++) {
-
       for ($j = 0; $j < count($series[$i]); $j++) {
         echo $series[$i][$j] ."<br>";
       }
