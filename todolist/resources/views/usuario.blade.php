3afeca20e77b3e6bb8783ba14afc74097ae10822
Rafael2026 <rafaaguilarmunoz@gmail.com>
2022-10-01 16:35:26 +0200

    Update dependencies and minor changes

--- todolist/resources/views/usuario.blade.php
+++ todolist/resources/views/usuario.blade.php
@@ -31,6 +31,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link href="../../dist/output.css" rel="stylesheet">
+  <link href="../../public/css/estilos.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
