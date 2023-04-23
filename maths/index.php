880384ad3e7fd3c09ceb182823977458ea425bcb
Rafael2026 <rafaaguilarmunoz@gmail.com>
2023-04-23 15:35:05 +0200

    Many changes

--- maths/index.php
+++ maths/index.php
@@ -1,6 +1,45 @@
 <?php
   error_reporting(E_ERROR);
   ini_set("display-errors", 0);
+
+  require_once("triangle/Triangle.php");
+  require_once("square/Square.php");
+  require_once("circle/Circle.php");
+  require_once("cube/Cube.php");
+
+  $flecha = new Triangle(2, 4);
+  $flecha2 = new Triangle(4, 8);
+  $flecha3 = new Triangle(3, 5);
+
+  $cuadro = new Square(2, 4);
+  $cuadro2 = new Square(4, 8);
+  $cuadro3 = new Square(3, 5);
+
+  $rueda = new Circle(2);
+  $rueda2 = new Circle(4);
+  $rueda3 = new Circle(3);
+
+  $cubo = new Cube(4);
+
+  $figuras2D = [$flecha, $cuadro, $rueda];
+  $figuras3D = [$cubo];
+
+  $array = array("Triangle", "Square", "Circle");
+
+  /*
+    echo "<pre>";
+    var_dump($array);
+    echo "</pre>";
+
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+
+    echo $array[0];
+
+    echo "<br><br>Longitud del array: ". count($array) . "<br>";
+  */
+
 ?>
 
 <!DOCTYPE html>
@@ -22,45 +61,7 @@
     <section>
 
       <?php
-
-        require_once("triangle/Triangle.php");
-        require_once("square/Square.php");
-        require_once("circle/Circle.php");
-        require_once("cube/Cube.php");
-
-        $flecha = new Triangle(2, 4);
-        $flecha2 = new Triangle(4, 8);
-        $flecha3 = new Triangle(3, 5);
-
-        $cuadro = new Square(2, 4);
-        $cuadro2 = new Square(4, 8);
-        $cuadro3 = new Square(3, 5);
-
-        $rueda = new Circle(2);
-        $rueda2 = new Circle(4);
-        $rueda3 = new Circle(3);
-
-        $cubo = new Cube(4);
-
-        $figuras2D = [$flecha, $cuadro, $rueda];
-        $figuras3D = [$cubo];
-
-        $array = array("Triangle", "Square", "Circle");
-
-        /*echo "<pre>";
-        var_dump($array);
-        echo "</pre>";
-
-        echo "<pre>";
-        print_r($array);
-        echo "</pre>";
-
-        echo $array[0];
-
-        echo "<br><br>Longitud del array: ". count($array) . "<br>";*/
-
         for ($i = 0; $i < count($array); $i++) {
-
       ?>
 
         <table>
@@ -91,8 +92,12 @@
 
             <tr>
               <td>
-                <label for="lado"></label>
-                <input type="number" min="1" name="lado" id="lado">
+                <form action="" method="post">
+                  <label for="lado">Lado:</label>
+                  <input type="number" min="1" name="lado" id="lado">
+
+                  <input type="button" value="Calcular">
+                </form>
               </td>
             </tr>
 
@@ -115,7 +120,6 @@
     <section>
 
       <?php
-
         for ($i = 0; $i < count($array); $i++) {
       ?>
 
@@ -147,15 +151,15 @@
 
             <tr>
               <td>
-                <label for="base">Base</label>
-                <input type="number" min="1" name="base" id="base">
-              </td>
-            </tr>
+                <form action="" method="post">
+                  <label for="base">Base:</label>
+                  <input type="number" min="1" name="base" id="base">
 
-            <tr>
-              <td>
-                <label for="altura">Altura</label>
-                <input type="number" min="1" name="altura" id="altura">
+                  <label for="altura">Altura:</label>
+                  <input type="number" min="1" name="altura" id="altura">
+
+                  <input type="button" value="Calcular">
+                </form>
               </td>
             </tr>
 
@@ -199,7 +203,7 @@
 
             <tr>
               <td>
-                <img src=<?php echo "img/". $cubo[$i] .".webp"; ?> alt="<?php echo $cubo[$i]; ?>">
+                <img src=<?php echo "img/". $cubo[$i] .".svg"; ?> alt="<?php echo $cubo[$i]; ?>">
               </td>
             </tr>
 
@@ -208,6 +212,21 @@
                 <p><?php echo $figuras3D[$i]->volume(); ?></p>
               </td>
             </tr>
+
+            <tr>
+              <td>
+                <form action="" method="post">
+                  <label for="base">Base:</label>
+                  <input type="number" min="1" name="base" id="base">
+
+                  <label for="altura">Altura:</label>
+                  <input type="number" min="1" name="altura" id="altura">
+
+                  <input type="button" value="Calcular">
+                </form>
+              </td>
+            </tr>
+
           </tbody>
 
         </table>
@@ -222,9 +241,9 @@
 
     </section>
 
-    <!--<section>
-      <img src="img/tabla.webp" alt="Tabla de figuras2D (Perímetros y áreas)">
-    </section>-->
+    <section>
+      <img src="img/tabla.webp" alt="Tabla de figuras (Perímetros, áreas y volúmenes)">
+    </section>
 
   </main>
 
